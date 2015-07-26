4383702f6f046fee597637a19797e57c976b8ff5
Baron Jon <baronjon@me.com>
2015-07-26 10:40:48 -1000

    deleted confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721083458) do
+ActiveRecord::Schema.define(version: 20150726171924) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
