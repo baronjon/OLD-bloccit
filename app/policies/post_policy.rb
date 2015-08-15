3b1f77332ddcb2f453b02609abce262c534b830f
Baron Jon <baronjon@me.com>
2015-08-15 10:33:13 -0400

    Assignment-authorizations

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,23 @@
 class PostPolicy < ApplicationPolicy
+	class Scope < Scope
+		#attr_reader :user, :scope
+
+		#def initialize(user, scope)
+		#	@user = user
+		#	@scope = scope
+		#end
+
+		def resolve
+			if (user.admin? || user.moderator?)
+				scope.all
+			else
+				scope.where(user: user)
+			end
+		end
+	end
+
+
+
 
 def index?
 	true
