e1b86a5936f607b931b40ec5c54f8f3e4c67ac4a
Baron Jon <baronjon@me.com>
2015-08-20 00:30:23 -0400

    assignment topicsAndPosts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'summaries/index'
+
+  get 'summaries/new'
+
+  get 'summaries/show'
+
+  get 'summaries/edit'
+
   devise_for :users
+
+  resources :summaries
   
   resources :topics do
   	resources :posts, except: [:index]
